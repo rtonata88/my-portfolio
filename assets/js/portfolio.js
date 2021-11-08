ee7af28933706460eb4f162797976440301c3465
rtonata88 <rtonata@gmail.com>
2021-11-09 00:06:58 +0200

    Fix Javascript Linter errors

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,10 +1,10 @@
-let closeMenu = document.querySelectorAll(".mobile-menu-toggle");
-closeMenu.forEach(function (menu) {
-  menu.addEventListener("click", function () {
-    let overlay = document.querySelector(".menu-overlay");
-    overlay.classList.toggle("hide-menu");
+const closeMenu = document.querySelectorAll('.mobile-menu-toggle');
+closeMenu.forEach((element) => {
+  element.addEventListener('click', () => {
+    const overlay = document.querySelector('.menu-overlay');
+    overlay.classList.toggle('hide-menu');
 
-    let hamburgerMenu = document.querySelector(".mobile-menu");
-    hamburgerMenu.classList.toggle("hide-hamburger");
+    const hamburgerMenu = document.querySelector('.mobile-menu');
+    hamburgerMenu.classList.toggle('hide-hamburger');
   });
 });
