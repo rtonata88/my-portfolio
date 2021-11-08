c5b3f647a38a1f108f2070c0fa09ca518ff29774
rtonata88 <rtonata@gmail.com>
2021-11-08 23:45:00 +0200

    Add navigation for mobile menus

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,5 +3,8 @@ closeMenu.forEach(function (menu) {
   menu.addEventListener("click", function () {
     let overlay = document.querySelector(".menu-overlay");
     overlay.classList.toggle("hide-menu");
+
+    let hamburgerMenu = document.querySelector(".mobile-menu");
+    hamburgerMenu.classList.toggle("hide-hamburger");
   });
 });
