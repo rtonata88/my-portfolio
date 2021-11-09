e5a72220db61252eadc866e57faa48c2be287986
karam084 <karam.ezz84@gmail.com>
2021-11-09 22:23:07 +0200

    Add popup button

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,10 +1,10 @@
-const closeMenu = document.querySelectorAll('.mobile-menu-toggle');
+const closeMenu = document.querySelectorAll(".mobile-menu-toggle");
 closeMenu.forEach((element) => {
-  element.addEventListener('click', () => {
-    const overlay = document.querySelector('.menu-overlay');
-    overlay.classList.toggle('hide-menu');
+  element.addEventListener("click", () => {
+    const overlay = document.querySelector(".menu-overlay");
+    overlay.classList.toggle("hide-menu");
 
-    const hamburgerMenu = document.querySelector('.mobile-menu');
-    hamburgerMenu.classList.toggle('hide-hamburger');
+    const hamburgerMenu = document.querySelector(".mobile-menu");
+    hamburgerMenu.classList.toggle("hide-hamburger");
   });
 });
