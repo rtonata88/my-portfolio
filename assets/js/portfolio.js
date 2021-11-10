be33e3831b311d492c7bc528954e14914652f1dc
rtonata88 <rtonata@gmail.com>
2021-11-10 17:45:30 +0200

    Add populate popup window with relevant information

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,4 +1,5 @@
 const closeMenu = document.querySelectorAll(".mobile-menu-toggle");
+
 closeMenu.forEach((element) => {
   element.addEventListener("click", () => {
     const overlay = document.querySelector(".menu-overlay");
@@ -11,13 +12,38 @@ closeMenu.forEach((element) => {
 
 const popup = document.getElementById("popup");
 
-// Get the button that opens the modal
-const seeProjectButton = document.querySelectorAll("see-project-button");
+
+const seeProjectButton = document.querySelectorAll(".see-project-button");
 seeProjectButton.forEach((element) => {
   element.addEventListener("click", () => {
     popup.style.display = "block";
+
+    const projectDetails = getProjectDetails(element.id);
+    document.getElementById('popup-project-title').innerHTML = projectDetails.name;
+    let details = '';
+    for(let key in projectDetails.details){
+      details += '<span class="client-name">'+projectDetails.details[key]+'</span>';
+      details += '<i class="fas fa-circle divider"></i>';
+    }
+    document.getElementById('popup-project-info').innerHTML = details;
+    document.getElementById('popup-project-image-holder').innerHTML =  projectDetails.image;
+    document.getElementById('popup-project-description').innerHTML = projectDetails.description;
+    
+    let popupProjectStack = ''; 
+    for(let key in projectDetails.technologies){
+      popupProjectStack += '<span class="label">'+projectDetails.technologies[key]+'</span>'
+    }
+    document.getElementById('popup-project-stack').innerHTML = popupProjectStack;
+    document.getElementById('see-live-btn').addEventListener("click", () => {
+      window.location.href=projectDetails.live_server;
+    });
+
+    document.getElementById('see-source-btn').addEventListener("click", () => {
+      window.location.href=projectDetails.source;
+    })
   });
 });
+
 /*
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
@@ -33,3 +59,107 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }*/
+
+
+  const getProjectDetails = (project) => {
+    const projectDetails = {
+        "Tonic" : {
+          "name": "Tonic",
+          "details" : [
+            "CANOPY",
+            "Backend",
+            "2015",
+            "Dev",
+          ],
+          "description": "Lorem Ipsum is simply dummy text of the printing and typesetting"+
+                  "industry. Lorem Ipsum has been the industry's standard dummy text"+
+                  "ever since the 1500s, when an unknown printer took a galley of type"+
+                  "and scrambled it 1960s with the releaLorem Ipsum is simply dummy"+
+                  "text of the printing and typesetting industry. Lorem Ipsum has been"+
+                  "the industry's standard dummy text ever since the 1500s, when an"+
+                  "unknown printer took a galley of type and scrambled it 1960s with"+
+                  "the releorem Ipsum is simply dummy text of the printing and"+
+                  "typesetting industry. Lorem Ipsum han printer took a galley of type",
+          "image": '<img src="./assets/images/project-01.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">', 
+          "technologies": [
+            "HTML", "Javascript", "CSS"
+          ], 
+          "live_server": "https://github.com/",
+          "source": "https://github.com/"
+        },
+
+        "360" : {
+          "name": "Facebook 360",
+          "details" : [
+            "Facebook",
+            "Fullstack dev",
+            "2015",
+          ],
+          "description": "Lorem Ipsum is simply dummy text of the printing and typesetting"+
+                  "industry. Lorem Ipsum has been the industry's standard dummy text"+
+                  "ever since the 1500s, when an unknown printer took a galley of type"+
+                  "and scrambled it 1960s with the releaLorem Ipsum is simply dummy"+
+                  "text of the printing and typesetting industry. Lorem Ipsum has been"+
+                  "the industry's standard dummy text ever since the 1500s, when an"+
+                  "unknown printer took a galley of type and scrambled it 1960s with"+
+                  "the releorem Ipsum is simply dummy text of the printing and"+
+                  "typesetting industry. Lorem Ipsum han printer took a galley of type",
+          "image": '<img src="./assets/images/project-02.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">', 
+          "technologies": [
+            "HTML", "Javascript", "CSS", "Ruby on Rails"
+          ], 
+          "live_server": "https://github.com/",
+          "source": "https://github.com/"
+        },
+
+        "Uber" : {
+          "name": "Uber Navigation",
+          "details" : [
+            "Uber",
+            "Lead Developer",
+            "2018",
+          ],
+          "description": "Lorem Ipsum is simply dummy text of the printing and typesetting"+
+                  "industry. Lorem Ipsum has been the industry's standard dummy text"+
+                  "ever since the 1500s, when an unknown printer took a galley of type"+
+                  "and scrambled it 1960s with the releaLorem Ipsum is simply dummy"+
+                  "text of the printing and typesetting industry. Lorem Ipsum has been"+
+                  "the industry's standard dummy text ever since the 1500s, when an"+
+                  "unknown printer took a galley of type and scrambled it 1960s with"+
+                  "the releorem Ipsum is simply dummy text of the printing and"+
+                  "typesetting industry. Lorem Ipsum han printer took a galley of type",
+          "image": '<img src="./assets/images/project-03.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">', 
+          "technologies": [
+            "HTML", "Javascript", "CSS", "Ruby on Rails"
+          ], 
+          "live_server": "https://github.com/",
+          "source": "https://github.com/"
+        },
+
+        "Stories" : {
+          "name": "Multi purpose stories",
+          "details" : [
+            "Facebook",
+            "Backend",
+            "2021",
+          ],
+          "description": "Lorem Ipsum is simply dummy text of the printing and typesetting"+
+                  "industry. Lorem Ipsum has been the industry's standard dummy text"+
+                  "ever since the 1500s, when an unknown printer took a galley of type"+
+                  "and scrambled it 1960s with the releaLorem Ipsum is simply dummy"+
+                  "text of the printing and typesetting industry. Lorem Ipsum has been"+
+                  "the industry's standard dummy text ever since the 1500s, when an"+
+                  "unknown printer took a galley of type and scrambled it 1960s with"+
+                  "the releorem Ipsum is simply dummy text of the printing and"+
+                  "typesetting industry. Lorem Ipsum han printer took a galley of type",
+          "image": '<img src="./assets/images/project-04.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">', 
+          "technologies": [
+            "HTML", "Javascript", "CSS", "Ruby on rails"
+          ], 
+          "live_server": "https://github.com/",
+          "source": "https://github.com/"
+        }
+    }
+
+    return projectDetails[project];
+  } 
