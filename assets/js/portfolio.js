0ca35b7ff014b6bfc4657056a5d151119c30afa3
karam084 <karam.ezz84@gmail.com>
2021-11-10 18:58:23 +0200

    Add close of popup window feature

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -12,154 +12,140 @@ closeMenu.forEach((element) => {
 
 const popup = document.getElementById("popup");
 
-
 const seeProjectButton = document.querySelectorAll(".see-project-button");
 seeProjectButton.forEach((element) => {
   element.addEventListener("click", () => {
     popup.style.display = "block";
 
     const projectDetails = getProjectDetails(element.id);
-    document.getElementById('popup-project-title').innerHTML = projectDetails.name;
-    let details = '';
-    for(let key in projectDetails.details){
-      details += '<span class="client-name">'+projectDetails.details[key]+'</span> ';
+    document.getElementById("popup-project-title").innerHTML =
+      projectDetails.name;
+    let details = "";
+    for (let key in projectDetails.details) {
+      details +=
+        '<span class="client-name">' + projectDetails.details[key] + "</span> ";
       details += ' <i class="fas fa-circle divider"></i> ';
     }
-    document.getElementById('popup-project-info').innerHTML = details;
-    document.getElementById('popup-project-image-holder').innerHTML =  projectDetails.image;
-    document.getElementById('popup-project-description').innerHTML = projectDetails.description;
-    
-    let popupProjectStack = ''; 
-    for(let key in projectDetails.technologies){
-      popupProjectStack += '<span class="label">'+projectDetails.technologies[key]+'</span>'
+    document.getElementById("popup-project-info").innerHTML = details;
+    document.getElementById("popup-project-image-holder").innerHTML =
+      projectDetails.image;
+    document.getElementById("popup-project-description").innerHTML =
+      projectDetails.description;
+
+    let popupProjectStack = "";
+    for (let key in projectDetails.technologies) {
+      popupProjectStack +=
+        '<span class="label">' + projectDetails.technologies[key] + "</span>";
     }
-    document.getElementById('popup-project-stack').innerHTML = popupProjectStack;
-    document.getElementById('see-live-btn').addEventListener("click", () => {
-      window.location.href=projectDetails.live_server;
+    document.getElementById("popup-project-stack").innerHTML =
+      popupProjectStack;
+    document.getElementById("see-live-btn").addEventListener("click", () => {
+      window.location.href = projectDetails.live_server;
     });
 
-    document.getElementById('see-source-btn').addEventListener("click", () => {
-      window.location.href=projectDetails.source;
-    })
+    document.getElementById("see-source-btn").addEventListener("click", () => {
+      window.location.href = projectDetails.source;
+    });
   });
 });
 
-/*
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+  popup.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }*/
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+};
 
+const getProjectDetails = (project) => {
+  const projectDetails = {
+    Tonic: {
+      name: "Tonic",
+      details: ["CANOPY", "Backend", "2015", "Dev"],
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting" +
+        "industry. Lorem Ipsum has been the industry's standard dummy text" +
+        "ever since the 1500s, when an unknown printer took a galley of type" +
+        "and scrambled it 1960s with the releaLorem Ipsum is simply dummy" +
+        "text of the printing and typesetting industry. Lorem Ipsum has been" +
+        "the industry's standard dummy text ever since the 1500s, when an" +
+        "unknown printer took a galley of type and scrambled it 1960s with" +
+        "the releorem Ipsum is simply dummy text of the printing and" +
+        "typesetting industry. Lorem Ipsum han printer took a galley of type",
+      image:
+        '<img src="./assets/images/project-01.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">',
+      technologies: ["HTML", "Javascript", "CSS"],
+      live_server: "https://github.com/",
+      source: "https://github.com/",
+    },
 
-  const getProjectDetails = (project) => {
-    const projectDetails = {
-        "Tonic" : {
-          "name": "Tonic",
-          "details" : [
-            "CANOPY",
-            "Backend",
-            "2015",
-            "Dev",
-          ],
-          "description": "Lorem Ipsum is simply dummy text of the printing and typesetting"+
-                  "industry. Lorem Ipsum has been the industry's standard dummy text"+
-                  "ever since the 1500s, when an unknown printer took a galley of type"+
-                  "and scrambled it 1960s with the releaLorem Ipsum is simply dummy"+
-                  "text of the printing and typesetting industry. Lorem Ipsum has been"+
-                  "the industry's standard dummy text ever since the 1500s, when an"+
-                  "unknown printer took a galley of type and scrambled it 1960s with"+
-                  "the releorem Ipsum is simply dummy text of the printing and"+
-                  "typesetting industry. Lorem Ipsum han printer took a galley of type",
-          "image": '<img src="./assets/images/project-01.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">', 
-          "technologies": [
-            "HTML", "Javascript", "CSS"
-          ], 
-          "live_server": "https://github.com/",
-          "source": "https://github.com/"
-        },
+    360: {
+      name: "Facebook 360",
+      details: ["Facebook", "Fullstack dev", "2015"],
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting" +
+        "industry. Lorem Ipsum has been the industry's standard dummy text" +
+        "ever since the 1500s, when an unknown printer took a galley of type" +
+        "and scrambled it 1960s with the releaLorem Ipsum is simply dummy" +
+        "text of the printing and typesetting industry. Lorem Ipsum has been" +
+        "the industry's standard dummy text ever since the 1500s, when an" +
+        "unknown printer took a galley of type and scrambled it 1960s with" +
+        "the releorem Ipsum is simply dummy text of the printing and" +
+        "typesetting industry. Lorem Ipsum han printer took a galley of type",
+      image:
+        '<img src="./assets/images/project-02.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">',
+      technologies: ["HTML", "Javascript", "CSS"],
+      live_server: "https://github.com/",
+      source: "https://github.com/",
+    },
 
-        "360" : {
-          "name": "Facebook 360",
-          "details" : [
-            "Facebook",
-            "Fullstack dev",
-            "2015",
-          ],
-          "description": "Lorem Ipsum is simply dummy text of the printing and typesetting"+
-                  "industry. Lorem Ipsum has been the industry's standard dummy text"+
-                  "ever since the 1500s, when an unknown printer took a galley of type"+
-                  "and scrambled it 1960s with the releaLorem Ipsum is simply dummy"+
-                  "text of the printing and typesetting industry. Lorem Ipsum has been"+
-                  "the industry's standard dummy text ever since the 1500s, when an"+
-                  "unknown printer took a galley of type and scrambled it 1960s with"+
-                  "the releorem Ipsum is simply dummy text of the printing and"+
-                  "typesetting industry. Lorem Ipsum han printer took a galley of type",
-          "image": '<img src="./assets/images/project-02.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">', 
-          "technologies": [
-            "HTML", "Javascript", "CSS"
-          ], 
-          "live_server": "https://github.com/",
-          "source": "https://github.com/"
-        },
+    Uber: {
+      name: "Uber Navigation",
+      details: ["Uber", "Lead Developer", "2018"],
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting" +
+        "industry. Lorem Ipsum has been the industry's standard dummy text" +
+        "ever since the 1500s, when an unknown printer took a galley of type" +
+        "and scrambled it 1960s with the releaLorem Ipsum is simply dummy" +
+        "text of the printing and typesetting industry. Lorem Ipsum has been" +
+        "the industry's standard dummy text ever since the 1500s, when an" +
+        "unknown printer took a galley of type and scrambled it 1960s with" +
+        "the releorem Ipsum is simply dummy text of the printing and" +
+        "typesetting industry. Lorem Ipsum han printer took a galley of type",
+      image:
+        '<img src="./assets/images/project-03.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">',
+      technologies: ["HTML", "Javascript", "CSS"],
+      live_server: "https://github.com/",
+      source: "https://github.com/",
+    },
 
-        "Uber" : {
-          "name": "Uber Navigation",
-          "details" : [
-            "Uber",
-            "Lead Developer",
-            "2018",
-          ],
-          "description": "Lorem Ipsum is simply dummy text of the printing and typesetting"+
-                  "industry. Lorem Ipsum has been the industry's standard dummy text"+
-                  "ever since the 1500s, when an unknown printer took a galley of type"+
-                  "and scrambled it 1960s with the releaLorem Ipsum is simply dummy"+
-                  "text of the printing and typesetting industry. Lorem Ipsum has been"+
-                  "the industry's standard dummy text ever since the 1500s, when an"+
-                  "unknown printer took a galley of type and scrambled it 1960s with"+
-                  "the releorem Ipsum is simply dummy text of the printing and"+
-                  "typesetting industry. Lorem Ipsum han printer took a galley of type",
-          "image": '<img src="./assets/images/project-03.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">', 
-          "technologies": [
-            "HTML", "Javascript", "CSS"
-          ], 
-          "live_server": "https://github.com/",
-          "source": "https://github.com/"
-        },
+    Stories: {
+      name: "Multi purpose stories",
+      details: ["Facebook", "Backend", "2021"],
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting" +
+        "industry. Lorem Ipsum has been the industry's standard dummy text" +
+        "ever since the 1500s, when an unknown printer took a galley of type" +
+        "and scrambled it 1960s with the releaLorem Ipsum is simply dummy" +
+        "text of the printing and typesetting industry. Lorem Ipsum has been" +
+        "the industry's standard dummy text ever since the 1500s, when an" +
+        "unknown printer took a galley of type and scrambled it 1960s with" +
+        "the releorem Ipsum is simply dummy text of the printing and" +
+        "typesetting industry. Lorem Ipsum han printer took a galley of type",
+      image:
+        '<img src="./assets/images/project-04.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">',
+      technologies: ["HTML", "Javascript", "CSS"],
+      live_server: "https://github.com/",
+      source: "https://github.com/",
+    },
+  };
 
-        "Stories" : {
-          "name": "Multi purpose stories",
-          "details" : [
-            "Facebook",
-            "Backend",
-            "2021",
-          ],
-          "description": "Lorem Ipsum is simply dummy text of the printing and typesetting"+
-                  "industry. Lorem Ipsum has been the industry's standard dummy text"+
-                  "ever since the 1500s, when an unknown printer took a galley of type"+
-                  "and scrambled it 1960s with the releaLorem Ipsum is simply dummy"+
-                  "text of the printing and typesetting industry. Lorem Ipsum has been"+
-                  "the industry's standard dummy text ever since the 1500s, when an"+
-                  "unknown printer took a galley of type and scrambled it 1960s with"+
-                  "the releorem Ipsum is simply dummy text of the printing and"+
-                  "typesetting industry. Lorem Ipsum han printer took a galley of type",
-          "image": '<img src="./assets/images/project-04.jpg" alt="Tonic project screenshot" class="popup-project-image" id="popup-project-image">', 
-          "technologies": [
-            "HTML", "Javascript", "CSS"
-          ], 
-          "live_server": "https://github.com/",
-          "source": "https://github.com/"
-        }
-    }
-
-    return projectDetails[project];
-  } 
+  return projectDetails[project];
+};
