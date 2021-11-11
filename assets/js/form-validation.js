4073a17f4d3bc9cc89249b91b2611cb73a8ec7d9
rtonata88 <rtonata@gmail.com>
2021-11-11 18:45:18 +0200

    Fix linter errors

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -8,11 +8,11 @@ const isEmailValid = (email) => {
 };
 
 const checkEmail = (email) => {
-  //console.log(isEmailValid(email));
+  // console.log(isEmailValid(email));
   let valid = true;
   if (!isEmailValid(email)) {
-      valid = false;
-  } 
+    valid = false;
+  }
   return valid;
 };
 
@@ -20,17 +20,17 @@ form.addEventListener('submit', (event) => {
   // prevent the form from submitting
   event.preventDefault();
 
-  const email = form.elements['email'];
+  const { email } = form.elements;
 
-  if(!checkEmail(email)){
+  if (!checkEmail(email)) {
     email.setCustomValidity(
-      'Please enter a valid email address and in lowercase letters.'
+      'Please enter a valid email address and in lowercase letters.',
     );
-    emailError.textContent = "Please enter a valid email address and in lowercase letters.";
+    emailError.textContent = 'Please enter a valid email address and in lowercase letters.';
   } else {
-     email.setCustomValidity('');
-     emailError.textContent = ''; // Reset the content of the message
-     emailError.className = 'error'; // Reset the visual state of the message
-     form.submit();
+    email.setCustomValidity('');
+    emailError.textContent = ''; // Reset the content of the message
+    emailError.className = 'error'; // Reset the visual state of the message
+    form.submit();
   }
 });
